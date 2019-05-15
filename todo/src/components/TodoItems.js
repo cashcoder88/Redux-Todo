f660a76f04ff84bd9c3844f37f3ab80327478d60
cashcoder88 <cashglobe@gmail.com>
2019-05-14 19:43:00 -0600

    Working on second component

---
+++ todo/src/components/TodoItems.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import toggleTodo from '../actions';
+import { Connect } from 'react-redux';
+
+
+class TodoItems extends React.Component {
+    render() {
+        return (
+            <li>
+                <input>
+                </input>
+            </li>
+        )
+    }
+}
