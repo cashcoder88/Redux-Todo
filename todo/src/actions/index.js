c96a93d6c388df882e0b20e66116e4b9e1095f75
cashcoder88 <cashglobe@gmail.com>
2019-05-14 18:50:09 -0600

    Finished actions for now

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,2 +1,17 @@
 export const ADD_TODO = 'ADD_TODO';
 export const TOGGLE_TODO = 'TOGGLE_TODO';
+
+
+export const addTodo = todo => {
+    return {
+        type: ADD_TODO,
+        payload: todo
+    }
+}
+
+export const toggleTodo = index => {
+    return {
+        type: TOGGLE_TODO,
+        payload: index
+    }
+}
