9a1a0654856d7481051007efd0261cd60f72c1a4
cashcoder88 <cashglobe@gmail.com>
2019-05-14 18:35:25 -0600

    React app created, building out skeleton of app

---
+++ todo/src/reducers/index.js
@@ -0,0 +1,12 @@
+import {} from '../actions';
+
+const initialState = {
+    todos: []
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        default:
+          return state;
+    }
+}
